98b32a76c0028aed7b717351b78cd003b427cea8
mxj9017 <mehul.jain31@mavs.uta.edu>
2017-04-04 19:31:22 -0500

    Update homework1.s

--- examples/homework1.s
+++ examples/homework1.s
@@ -5,12 +5,17 @@ main:
     BL  _prompt             @ branch to prompt procedure with return
     BL  _scanf              @ branch to scanf procedure with return for first variable
     MOV R1, R0              @ move return value R0 to argument register R1
+    BL _printf                
+    
     BL  _prompt2             @ branch to prompt procedure with return
-    BL_scanftwo               @ branch to scanf for another variable
-    MOV R3,R0               @ move return value R0 to argument register R2
+    BL_scanf2               @ branch to scanf for another variable
+    MOV R1,R0               @ move return value R0 to argument register R2
+    BL_printf
+    
     BL  _prompt3             @ branch to prompt procedure with return
-    BL_scanfthree               @ branch to scanf the symbol for calculation
-    MOV R4, R0              @ move return value R0 to argument register R4
+    BL_scanf3               @ branch to scanf the symbol for calculation
+    MOV R1, R0              @ move return value R0 to argument register R4
+    Bl_printf
     
     
    
@@ -41,15 +46,15 @@ _prompt2:
     MOV R7, #4              @ write syscall, 4
     MOV R0, #1              @ output stream to monitor, 1
     MOV R2, #31             @ print string length
-    LDR R3, =prompt_str1     @ string at label prompt_str:
+    LDR R1, =prompt_str1     @ string at label prompt_str:
     SWI 0                   @ execute syscall
     MOV PC, LR              @ return
     
-_scanftwo:
+_scanf2:
     PUSH {LR}               @ store LR since scanf call overwrites
     SUB SP, SP, #4          @ make room on stack
     LDR R0, =format_str     @ R0 contains address of format string
-    MOV R3, SP              @ move SP to R1 to store entry on stack
+    MOV R1, SP              @ move SP to R1 to store entry on stack
     BL scanf                @ call scanf
     LDR R0, [SP]            @ load value at SP into R0
     ADD SP, SP, #4          @ restore the stack pointer
@@ -60,21 +65,28 @@ _prompt3:
     MOV R7, #4              @ write syscall, 4
     MOV R0, #1              @ output stream to monitor, 1
     MOV R2, #31             @ print string length
-    LDR R3, =symbol_input   @ string at label prompt_str:
+    LDR R1, =symbol_input   @ string at label prompt_str:
     SWI 0                   @ execute syscall
     MOV PC, LR              @ return
 
-_scanfthree:
+_scanf3:
 
     PUSH {LR}               @ store LR since scanf call overwrites
     SUB SP, SP, #4          @ make room on stack
     LDR R0, =format_str     @ R0 contains address of format string
-    MOV R4, SP              @ move SP to R1 to store entry on stack
+    MOV R1, SP              @ move SP to R1 to store entry on stack
     BL scanf                @ call scanf
     LDR R0, [SP]            @ load value at SP into R0
     ADD SP, SP, #4          @ restore the stack pointer
     POP {PC}                @ return
     
+_printf:
+   _printf:
+    MOV R4, LR              @ store LR since printf call overwrites
+    LDR R0, =printf_str     @ R0 contains formatted string address
+    MOV R1, R1              @ R1 contains printf argument (redundant line)
+    BL printf               @ call printf
+    MOV PC, R4              @ return    
 
 .data
 format_str:     .asciz      "%d"
