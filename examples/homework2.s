a9bfb63cb88c9d9c062e75450beb32d733c255fd
mxj9017 <mehul.jain31@mavs.uta.edu>
2017-04-04 20:32:36 -0500

    Update homework2.s

--- examples/homework2.s
+++ examples/homework2.s
@@ -70,5 +70,4 @@ _scanf1:
 format_str:     .asciz      "%d"
 prompt_str1:     .asciz      "  "
 prompt_str2:    .asciz       "  "
-printf_str:     .asciz       "\nThe number entered was: %d\n"
 exit_str:     .ascii         "Terminating Program\n"
