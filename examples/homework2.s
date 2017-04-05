d7523d5be438a0a3250ad8eecd37c9cc5bcdb902
mxj9017 <mehul.jain31@mavs.uta.edu>
2017-04-04 21:35:16 -0500

    Update homework2.s

--- examples/homework2.s
+++ examples/homework2.s
@@ -7,6 +7,11 @@ main:
     MOV R1, R0              @ move return value R0 to argument register R1
                   
     
+    BL  _prompt             @ branch to printf procedure with return
+    BL  _getchar            @ branch to scanf procedure with return
+    MOV R3, R0              @ move return value R0 to argument register R1
+    BL  _compare1            @ check the scanf input
+    
     BL  _prompt2             @ branch to prompt procedure with return
     BL _scanf2               @ branch to scanf for another variable
     MOV R2,R0               @ move return value R0 to argument register R2
@@ -30,7 +35,72 @@ _prompt1:
     LDR R1, =prompt_str1     @ string at label prompt_str:
     SWI 0                   @ execute syscall
     MOV PC, LR              @ return
-       
+
+_prompt:
+    MOV R7, #4              @ write syscall, 4
+    MOV R0, #1              @ output stream to monitor, 1
+    MOV R4, #23             @ print string length
+    LDR R1, =prompt_str     @ string at label prompt_str:
+    SWI 0                   @ execute syscall
+    MOV PC, LR              @ return
+    
+ _getchar:
+    MOV R7, #3              @ write syscall, 3
+    MOV R0, #0              @ input stream from monitor, 0
+    MOV R2, #1              @ read a single character
+    LDR R3, =read_char      @ store the character in data memory
+    SWI 0                   @ execute the system call
+    LDR R0, [R3]            @ move the character to the return register
+    AND R0, #0xFF           @ mask out all but the lowest 8 bits
+    MOV PC, LR              @ return
+    
+    
+  _compare1:
+    CMP R3, #'+'            @ compare against the constant char '+'
+    BEQ _add                @ branch to addition handler
+    BNE _-compare2          @ branch to subtract check handler
+    
+    _add:
+    MOV R5, LR              @ store LR since printf call overwrites
+    LDR R0, =equal_str      @ R0 contains formatted string address
+    BL printf               @ call printf
+    MOV PC, R5              @ return
+
+_-compare2:
+    CMP R3, #'-'            @ compare against the constant char '-'
+    BEQ _subtract            @ branch to subtract handler
+    BNE _*compare3             @ branch to multiply check 
+    
+ _subtract:
+    MOV R5, LR              @ store LR since printf call overwrites
+    LDR R0, =equal_str      @ R0 contains formatted string address
+    BL printf               @ call printf
+    MOV PC, R5              @ return
+    
+  _-compare3:
+    CMP R3, #'*'            @ compare against the constant char '*'
+    BEQ _multiply            @ branch to multiply handler
+    BNE _*compare4            @ branch to multiply check 
+    
+ _multiply:
+    MOV R5, LR              @ store LR since printf call overwrites
+    LDR R0, =equal_str      @ R0 contains formatted string address
+    BL printf               @ call printf
+    MOV PC, R5              @ return
+    
+_-compare4:
+    CMP R3, #'M'            @ compare against the constant char 'M'
+    BEQ _maximum            @ branch to maximum handler
+    BNE _incorrect          @ branch to not equal handler
+    
+_incorrect:
+    MOV R5, LR              @ store LR since printf call overwrites
+    LDR R0, =nequal_str     @ R0 contains formatted string address
+    BL printf               @ call printf
+    MOV PC, R5              @ return
+    
+
+
 _prompt2:
     MOV R7, #4              @ write syscall, 4
     MOV R0, #1              @ output stream to monitor, 1
@@ -68,5 +138,9 @@ _scanf1:
 .data
 format_str:     .asciz      "%d"
 prompt_str1:     .asciz      "  "
-prompt_str2:    .asciz       "  "
+read_char:      .ascii      " "
+prompt_str:     .ascii      "Enter the +/-/*/M character: "
+equal_str:      .asciz      "CORRECT \n"
+nequal_str:     .asciz      "INCORRECT: %c \n"ompt_str2:    .asciz       "  "
+
 
