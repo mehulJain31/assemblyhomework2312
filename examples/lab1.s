f8fb9a8a5f4719d7b28528f8bac0ee2aa3f47dbd
mxj9017 <mehul.jain31@mavs.uta.edu>
2017-04-05 15:27:38 -0500

    Update lab1.s

--- examples/lab1.s
+++ examples/lab1.s
@@ -1,20 +1,19 @@
 .global main
 .func main
-
+   
 main:
     BL  _prompt1             @ branch to prompt procedure with return
     BL  _scanf1              @ branch to scanf procedure with return
     MOV R1, R0              @ move return value R0 to argument register R1
-    BL  _printf             @ branch to print procedure with return
-   
-   BL  _prompt2             @ branch to prompt procedure with return
-    BL _scanf2               @ branch to scanf for the second variable
-    MOV R2,R0               @ move return value R0 to argument register R2
-    BL _printf
-   
-   
-   
-   B   _exit               @ branch to exit procedure with no return
+    BL  _printf1             @ branch to print procedure with return
+ 
+    BL  _prompt2             @ branch to prompt procedure with return
+    BL  _scanf2              @ branch to scanf procedure with return
+    MOV R1, R0              @ move return value R0 to argument register R1
+    BL  _printf2             @ branch to print procedure with return
+ 
+ 
+    B   _exit               @ branch to exit procedure with no return
    
 _exit:  
     MOV R7, #4              @ write syscall, 4
@@ -28,19 +27,18 @@ _exit:
 _prompt1:
     MOV R7, #4              @ write syscall, 4
     MOV R0, #1              @ output stream to monitor, 1
-    MOV R4, #31             @ print string length
-    LDR R1, =prompt_str1     @ string at label prompt_str:
+    MOV R2, #31             @ print string length
+    LDR R1, =prompt_str     @ string at label prompt_str:
     SWI 0                   @ execute syscall
     MOV PC, LR              @ return
+       
+_printf1:
+    MOV R4, LR              @ store LR since printf call overwrites
+    LDR R0, =printf_str     @ R0 contains formatted string address
+    MOV R1, R1              @ R1 contains printf argument (redundant line)
+    BL printf               @ call printf
+    MOV PC, R4              @ return
     
-_prompt2:
-    MOV R7, #4              @ write syscall, 4
-    MOV R0, #1              @ output stream to monitor, 1
-    MOV R4, #31             @ print string length
-    LDR R2, =prompt_str2     @ string at label prompt_str:
-    SWI 0                   @ execute syscall
-    MOV PC, LR              @ return
-          
 _scanf1:
     PUSH {LR}                @ store LR since scanf call overwrites
     SUB SP, SP, #4          @ make room on stack
@@ -51,27 +49,35 @@ _scanf1:
     ADD SP, SP, #4          @ restore the stack pointer
     POP {PC}                 @ return
     
-    
- _scanf2:
-    PUSH {LR}                @ store LR since scanf call overwrites
-    SUB SP, SP, #4          @ make room on stack
-    LDR R0, =format_str     @ R0 contains address of format string
-    MOV R2, SP              @ move SP to R1 to store entry on stack
-    BL scanf                @ call scanf
-    LDR R0, [SP]            @ load value at SP into R0
-    ADD SP, SP, #4          @ restore the stack pointer
-    POP {PC}                 @ return
-    
-_printf:
+ _prompt2:
+    MOV R7, #4              @ write syscall, 4
+    MOV R0, #1              @ output stream to monitor, 1
+    MOV R2, #31             @ print string length
+    LDR R1, =prompt_str2     @ string at label prompt_str:
+    SWI 0                   @ execute syscall
+    MOV PC, LR              @ return
+       
+_printf2:
     MOV R4, LR              @ store LR since printf call overwrites
     LDR R0, =printf_str     @ R0 contains formatted string address
     MOV R1, R1              @ R1 contains printf argument (redundant line)
     BL printf               @ call printf
-    MOV PC, R4              @ return   
+    MOV PC, R4              @ return
+    
+_scanf2:
+    PUSH {LR}                @ store LR since scanf call overwrites
+    SUB SP, SP, #4          @ make room on stack
+    LDR R0, =format_str     @ R0 contains address of format string
+    MOV R1, SP              @ move SP to R1 to store entry on stack
+    BL scanf                @ call scanf
+    LDR R0, [SP]            @ load value at SP into R0
+    ADD SP, SP, #4          @ restore the stack pointer
+    POP {PC}                 @ return
+
 
 .data
-format_str:      .asciz      "%d"
-prompt_str1:     .asciz      "Type a number and press enter: "
-prompt_str2:     .asciz      "Type a number and press enter: "
-printf_str:      .asciz      "The number entered was: %d\n"
-exit_str:        .ascii      "Terminating program.\n"
+format_str:     .asciz      "%d"
+prompt_str:     .asciz      "Type a number and press enter: "
+prompt_str2:     .asciz      "Type a 2nd number and press enter: "
+printf_str:     .asciz      "The number entered was: %d\n"
+exit_str:       .ascii      "Terminating program.\n"
