4ab8be6f728e0f6538e8b024bd2f7e5306904672
mxj9017 <mehul.jain31@mavs.uta.edu>
2017-04-06 02:48:17 -0500

    Update p1_1001229017.s

--- p1_1001229017.s
+++ p1_1001229017.s
@@ -4,19 +4,17 @@
 
 main:
    BL _scanf      @take the first input
-   MOV R9,R0      @first variable in R9
+   MOV R9,R0      @first variable stored in R9
    BL _getchar    @take the symbol input
-   MOV R10,R0     @ symbol in R10
+   MOV R10,R0     @ symbol stored in R10
    BL _scanf      @call scanf for second input
-   MOV R11,R0     @second variable in R11
-   BL _compare    @compare function for determining what function to use
+   MOV R11,R0     @second variable stored in R11
+   BL _compare    @compare function for determining what manipulation to use
    MOV R1,R0      @move the temporary R0 to R1 for printing the result
    BL _print      @print the answer
-   BL main        @loop until the user wants
+   BL main        @loop until the user wants to enter the variables
    
    
-  
- 
 _scanf:
     MOV R4, LR                           @ store LR since printf call overwrites
     SUB SP, SP, #4                       @ make room on stack
@@ -27,67 +25,63 @@ _scanf:
     ADD SP, SP, #4                       @ restore the stack pointer
     MOV PC, R4                           @ return
    
-  _getchar:
+_getchar:
     MOV R7, #3              @ write syscall, 3
     MOV R0, #0              @ input stream from monitor, 0
     MOV R2, #1              @ read a single character
-    LDR R1, =read_symbol      @ store the character in data memory
+    LDR R1, =read_symbol    @ store the character in data memory
     SWI 0                   @ execute the system call
     LDR R0, [R1]            @ move the character to the return register
     AND R0, #0xFF           @ mask out all but the lowest 8 bits
     MOV PC, LR              @ return
     
-    _compare:
-       PUSH {LR}            @stack manipulation
-       CMP R10,#'+'         @compare for +
-       BEQ _addition        @ call the function
+_compare:
+    PUSH {LR}            @stack manipulation
+    
+    CMP R10,#'+'         @compare for +
+    BEQ _addition        @ call the function
        
-       CMP R10,#'-'
-       BEQ _subtract
+    CMP R10,#'-'         @compare for -
+    BEQ _subtract
        
-       CMP R10,#'*'
-       BEQ _product
+    CMP R10,#'*'         @compare for *
+    BEQ _product
        
-       CMP R10,#'M'
-       BEQ _maximum
+    CMP R10,#'M'         @compare for M (Maximum)
+    BEQ _maximum
        
-       POP {PC}
+    POP {PC}
        
-    _addition:
-      
+_addition:   
       PUSH {LR}          @open stack
       ADD R0,R9,R11      @ Add them
       POP {PC}
       
-     _subtract:
-      
+_subtract:
       PUSH {LR}         @open stack
-      SUB R0,R9,R11     @ subtract them
+      SUB R0,R9,R11     @Subtract them
       POP {PC}
       
-      _product:
-      PUSH {LR}       @open stack
-      MUL R0,R9,R11   @ multiply them
+_product:
+      PUSH {LR}        @open stack
+      MUL R0,R9,R11    @ multiply them
       POP {PC}
       
-      _maximum:
+_maximum:
       PUSH {LR}      @open stack
       CMP R9,R11     @compare the user values
       MOVGT R11,R9   @ assign R11, R9 if R9>R11
       MOV R0,R11     @ Move the greater number to R0  for printing the result
       POP {PC}
-      
-     
-     _print:
+          
+ _print:
     PUSH {LR}               @ store the return address
     LDR R0, =printf_str     @ R0 contains formatted string address
     MOV R1, R1              @ R1 contains printf argument 1 (redundant line)
     BL printf               @ call printf
     POP {PC}                @ restore the stack pointer and return
       
-      
-     
-    .data
+ .data
     format_str:	   .asciz	"%d"
     read_symbol:	   .ascii	" "
     printf_str:      .asciz	"%d\n"
