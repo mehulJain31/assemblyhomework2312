9a8ca47dd0631850ac6fb38bf8923b6d72037f47
mxj9017 <mehul.jain31@mavs.uta.edu>
2017-04-19 17:38:03 -0500

    Update p3_1001229017.s

--- p3_1001229017.s
+++ p3_1001229017.s
@@ -2,14 +2,17 @@
 .func main
 
 main:
-   BL _scanf      @take the first input
-   MOV R1,R0      @first variable stored in R9
-   BL _scanf      @call scanf for second input
-   MOV R2,R0     @second variable stored in R11
+   BL _scanf           @take the first input
+   MOV R1,R0           @first variable stored in R1
+   BL _scanf           @call scanf for second input
+   MOV R2,R0           @second variable stored in R2
+   BL _countpartitions @branch to the function   
    
    
+  BL _counterpartitions:
    
-   _scanf:
+  
+  _scanf:
     MOV R4, LR                           @ store LR since printf call overwrites
     SUB SP, SP, #4                       @ make room on stack
     LDR R0, =format_str                  @ R0 contains address of format string 
