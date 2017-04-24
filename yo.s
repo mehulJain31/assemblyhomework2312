2449a99ec3d7071bc9948e3cb031897ae7ebb868
mxj9017 <mehul.jain31@mavs.uta.edu>
2017-04-24 00:30:13 -0500

    Update yo.s

--- yo.s
+++ yo.s
@@ -1,87 +1,85 @@
 .global main
 .func main
-
-
-main:
-   BL _scanf      @take the first input
-   MOV R9,R0      @first variable stored in R9
-   BL _getchar    @take the symbol input
-   MOV R10,R0     @ symbol stored in R10
-   BL _scanf      @call scanf for second input
-   MOV R11,R0     @second variable stored in R11
-   BL _compare    @compare function for determining what manipulation to use
-   MOV R1,R0      @move the temporary R0 to R1 for printing the result
-   BL _print      @print the answer
-   BL main        @loop until the user wants to enter the variables
    
+main:
+    PUSH {R3}
+    PUSH {R4}  @ backup R4 and R5
+    PUSH {R5}
+
+    BL  _scanf              @ branch to scanf procedure with return
+    MOV R1, R0	       	@ obtained first number in R1
+    
+    PUSH {R1}
+    BL  _scanf		     @ _scanf for second number here 
+    MOV R2, R0          @ obtained second number in R2
+    POP {R1}
+ 
+    PUSH {R1}
+    PUSH {R2}
+
+    BL count_partitions
+    
+    POP {R2}
+    POP {R1}
+    
+    @ code to enter calculated result in R1, N in R2, M in R3
+    
+    MOV R3, R2
+    MOV R2, R1    
+    MOV R1, R0
+     
+    BL _print_result
+      
+    POP {R5}
+    POP {R4}
+    POP {R3}
+    B main
+
+count_partitions:
+	PUSH {LR}
+	
+	CMP R1, #0
+	MOVEQ R0, #1
+	POPEQ {PC}
+	MOVLT R0, #0
+	POPLT {PC}
+	
+	CMP R2, #0
+	MOVEQ R0, #0
+	POPEQ {PC}
+	
+	PUSH {R1}
+	SUB R1, R1, R2
+	BL count_partitions
+	POP {R1}
+	PUSH {R2}
+	PUSH {R0}
+	SUB R2, R2, #1
+	BL count_partitions
+	POP {R3}
+	ADD R0, R0, R3
+	POP {R2}
+	POP {PC}
    
 _scanf:
-    MOV R4, LR                           @ store LR since printf call overwrites
-    SUB SP, SP, #4                       @ make room on stack
-    LDR R0, =format_str                  @ R0 contains address of format string 
-    MOV R1, SP                           @ move SP to R1 to store entry on stack 
-    BL scanf                             @ call scanf
-    LDR R0, [SP]                         @ load value at SP into R0
-    ADD SP, SP, #4                       @ restore the stack pointer
-    MOV PC, R4                           @ return
-   
-_getchar:
-    MOV R7, #3              @ write syscall, 3
-    MOV R0, #0              @ input stream from monitor, 0
-    MOV R2, #1              @ read a single character
-    LDR R1, =read_symbol    @ store the character in data memory
-    SWI 0                   @ execute the system call
-    LDR R0, [R1]            @ move the character to the return register
-    AND R0, #0xFF           @ mask out all but the lowest 8 bits
-    MOV PC, LR              @ return
-    
-_compare:
-    PUSH {LR}            @stack manipulation
-    
-    CMP R10,#'+'         @compare for +
-    BEQ _addition        @ call the function
-       
-    CMP R10,#'-'         @compare for -
-    BEQ _subtract
-       
-    CMP R10,#'*'         @compare for *
-    BEQ _product
-       
-    CMP R10,#'M'         @compare for M (Maximum)
-    BEQ _maximum
-       
-    POP {PC}
-       
-_addition:   
-      PUSH {LR}          @open stack
-      ADD R0,R9,R11      @ Add them
-      POP {PC}
-      
-_subtract:
-      PUSH {LR}         @open stack
-      SUB R0,R9,R11     @Subtract them
-      POP {PC}
-      
-_product:
-      PUSH {LR}        @open stack
-      MUL R0,R9,R11    @ multiply them
-      POP {PC}
-      
-_maximum:
-      PUSH {LR}      @open stack
-      CMP R9,R11     @compare the user values
-      MOVGT R11,R9   @ assign R11, R9 if R9>R11
-      MOV R0,R11     @ Move the greater number to R0  for printing the result
-      POP {PC}
-          
- _print:
-    PUSH {LR}               @ store the return address
-    LDR R0, =printf_str     @ R0 contains formatted string address
-    MOV R1, R1              @ R1 contains printf argument 1 (redundant line)
-    BL printf               @ call printf
-    POP {PC}                @ restore the stack pointer and return
-      
- .data
-    format_str:	   .asciz	"%d"
-    read_symbol:	   .ascii	" "
-    printf_str:      .asciz	"%d\n"
+    PUSH {LR}                @ store LR since scanf call overwrites
+    SUB SP, SP, #4          @ make room on stack
+    LDR R0, =format_str     @ R0 contains address of format string
+    MOV R1, SP              @ move SP to R1 to store entry on stack
+    BL scanf                @ call scanf
+    LDR R0, [SP]            @ load value at SP into R0
+    ADD SP, SP, #4          @ restore the stack pointer
+    POP {PC}                 @ return
+
+_print_result:
+	PUSH {LR}
+        LDR  R0, =result_str
+        BL printf
+	POP {PC}
+        
+
+.data
+format_str:     .asciz      "%d"
+format_str1:    .asciz      "%d"
+exit_str:       .ascii      "Terminating program.\n"
+result_str:     .asciz      "There are %d partitions of %d using integers up to %d.\n"
