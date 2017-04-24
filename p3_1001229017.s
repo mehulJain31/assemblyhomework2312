74588fd26fcc6bce9c3100cbf39fc2eb9ca8cb5e
mxj9017 <mehul.jain31@mavs.uta.edu>
2017-04-24 00:12:02 -0500

    Update p3_1001229017.s

--- p3_1001229017.s
+++ p3_1001229017.s
@@ -6,9 +6,12 @@ main:
    MOV R1,R0           @first variable stored in R1
    BL _scanf           @call scanf for second input
    MOV R2,R0           @second variable stored in R2
-   BL count_partitions @branch to the function   
+   BL count_partitions @branch to the function  
+   Bl _printf
+   
    
    count_partitions:
+      PUSH {LR}
       CMP R1,#0
       MOVEQ R0,#1    @if n=0, return 1
       MOVLT R0,#0  @ if n<0 return 0
@@ -16,9 +19,23 @@ main:
       CMP R2,#0      @ compare m with 0
       MOVEQ R0,#0
       
-                      @last else with recursion
- 
+      PUSH{R1}      @last else with recursion
+      SUB R1,R1,R2
+      BL count_partitions
+      MOV R3,R0
+      POP{R1}
+      PUSH{R2}
+      SUB R2,R2,#1
+      BL count_partitions
+      ADD R0,R0,R3
+      POP{R2}
+      POP{PC}
    
+  _printf:
+    MOV R4, LR              @ store LR since printf call overwrites
+    LDR R0,R1,R2 =printf_str     @ R0 contains formatted string address
+    BL printf               @ call printf
+    MOV PC, R4              @ return
   
   _scanf:
     MOV R4, LR                           @ store LR since printf call overwrites
@@ -33,4 +50,5 @@ main:
 
 .data
     format_str:	   .asciz	"%d"
+    printf_str:     .asciz      "There are %d partitions of %d using integers up to %d\n"
     
