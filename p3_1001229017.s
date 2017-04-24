3e09577a6dff601ae07019ee0746c261624b0592
mxj9017 <mehul.jain31@mavs.uta.edu>
2017-04-24 00:41:05 -0500

    Update p3_1001229017.s

--- p3_1001229017.s
+++ p3_1001229017.s
@@ -8,15 +8,15 @@ main:
    BL _scanf           @call scanf for second input
    MOV R2, R0           @second variable stored in R2
    
-   PUSH{R1}
-   PUSH{R2}
+   PUSH {R1}
+   PUSH {R2}
    BL count_partitions @branch to the function  
    
    
   
    
-    POP{R2}
-    POP{R1}
+    POP {R2}
+    POP {R1}
     
     MOV R1,R0
     
@@ -29,14 +29,14 @@ main:
       
       CMP R1,#0
       MOVEQ R0,#1    @if n=0, return 1
-      POPEQ{PC}
+      POPEQ {PC}
       
       MOVLT R0,#0  @ if n<0 return 0
-      POPLT{PC}
+      POPLT {PC}
       
       CMP R2,#0      @ compare m with 0
       MOVEQ R0,#0
-      POPEQ{PC}
+      POPEQ {PC}
       
       PUSH {R1}      @last else with recursion
       SUB R1,R1,R2
