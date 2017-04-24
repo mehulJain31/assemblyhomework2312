9b0f11aa2a103996149e7ac8e1ce23ac82b85170
mxj9017 <mehul.jain31@mavs.uta.edu>
2017-04-24 00:43:42 -0500

    Update p3_1001229017.s

--- p3_1001229017.s
+++ p3_1001229017.s
@@ -61,14 +61,15 @@ main:
 	POP {PC}
   
   _scanf:
-    MOV R4, LR                           @ store LR since printf call overwrites
-    SUB SP, SP, #4                       @ make room on stack
-    LDR R0, =format_str                  @ R0 contains address of format string 
-    MOV R1, SP                           @ move SP to R1 to store entry on stack 
-    BL scanf                             @ call scanf
-    LDR R0, [SP]                         @ load value at SP into R0
-    ADD SP, SP, #4                       @ restore the stack pointer
-    MOV PC, R4                           @ return
+    PUSH {LR}                @ store LR since scanf call overwrites
+    SUB SP, SP, #4          @ make room on stack
+    LDR R0, =format_str     @ R0 contains address of format string
+    MOV R1, SP              @ move SP to R1 to store entry on stack
+    BL scanf                @ call scanf
+    LDR R0, [SP]            @ load value at SP into R0
+    ADD SP, SP, #4          @ restore the stack pointer
+    POP {PC}                 @ return
+
 
 
 .data
