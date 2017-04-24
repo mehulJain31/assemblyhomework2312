7058be4cc8d7e4d5bb3ff8496b2da142785408de
mxj9017 <mehul.jain31@mavs.uta.edu>
2017-04-24 00:39:29 -0500

    Update p3_1001229017.s

--- p3_1001229017.s
+++ p3_1001229017.s
@@ -4,10 +4,24 @@
 main:
    BL _scanf           @take the first input
    MOV R1, R0           @first variable stored in R1
+   
    BL _scanf           @call scanf for second input
    MOV R2, R0           @second variable stored in R2
+   
+   PUSH{R1}
+   PUSH{R2}
    BL count_partitions @branch to the function  
-   Bl _printf
+   
+   
+  
+   
+    POP{R2}
+    POP{R1}
+    
+    MOV R1,R0
+    
+    
+    B main                          @run the code again
    
    
    count_partitions:
@@ -15,19 +29,23 @@ main:
       
       CMP R1,#0
       MOVEQ R0,#1    @if n=0, return 1
+      POPEQ{PC}
+      
       MOVLT R0,#0  @ if n<0 return 0
+      POPLT{PC}
       
       CMP R2,#0      @ compare m with 0
       MOVEQ R0,#0
+      POPEQ{PC}
       
       PUSH {R1}      @last else with recursion
       SUB R1,R1,R2
       BL count_partitions
-      MOV R3,R0
       POP {R1}
       
-
-      PUSH{R0}
+      
+      PUSH {R2}
+      PUSH {R0}
       SUB R2,R2,#1
       BL count_partitions
       ADD R0,R0,R3
@@ -36,10 +54,10 @@ main:
       POP {PC}
    
   _printf:
-    MOV R4, LR              @ store LR since printf call overwrites
-    LDR R0, =printf_str     @ R0 contains formatted string address
-    BL printf               @ call printf
-    MOV PC, R4              @ return
+	PUSH {LR}
+        LDR  R0, =printf_str
+        BL printf
+	POP {PC}
   
   _scanf:
     MOV R4, LR                           @ store LR since printf call overwrites
