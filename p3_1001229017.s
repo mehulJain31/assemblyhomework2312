0286c98c50512eec117f9fe6860515f476bd3a55
mxj9017 <mehul.jain31@mavs.uta.edu>
2017-04-24 15:52:22 -0500

    Update and rename yo1.s to p3_1001229017.s

--- p3_1001229017.s
+++ p3_1001229017.s
@@ -64,14 +64,14 @@ main:
 	POP {PC}
   
   _scanf:
-    PUSH {LR}                @ store LR since scanf call overwrites
+    PUSH {LR}               @ store LR since scanf call overwrites
     SUB SP, SP, #4          @ make room on stack
     LDR R0, =format_str     @ R0 contains address of format string
     MOV R1, SP              @ move SP to R1 to store entry on stack
     BL scanf                @ call scanf
     LDR R0, [SP]            @ load value at SP into R0
     ADD SP, SP, #4          @ restore the stack pointer
-    POP {PC}                 @ return
+    POP {PC}                @ return
     
   
  .data
